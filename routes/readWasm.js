83480efb25c9e2810803bdcf1836f3903a455ca7
Hammad-Mubeen <hammad.mubeen@scytalelabs.com>
2022-04-20 12:58:42 +0500

    readWasm endpoint updated with serilization

--- routes/readWasm.js
+++ routes/readWasm.js
@@ -2,17 +2,23 @@ require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 var routerJsClient=require('../JsClients/ROUTER/src/utils')
+//const v8 = require('v8');
+var serialize = require('serialize-javascript');
 
 router.route("/getWasmData").get(async function (req, res, next) {
   try {
 
     let wasmData= routerJsClient.getBinary('JsClients/ROUTER/wasm/purse-proxy.wasm');
-    console.log(wasmData)
- 
+    console.log(wasmData);
+    //console.log(v8.serialize(wasmData));
+    console.log(serialize({ arr  : wasmData}));
+    //JSON.parse(JSON.stringify(wasmData));
+    serialize({ arr  : wasmData});
     return res.status(200).json({
         success: true,
         message: "Wasm data successfully read and converted. ",
-        wasmData: wasmData,
+        // wasmData: v8.serialize(wasmData),
+        wasmData: serialize({ arr  : wasmData}),
     });
     
   } catch (error) {
